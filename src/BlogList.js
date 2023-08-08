7a41cb29aef1d6f1cc6e4df4a5213636f9a6f82e
canbatuhans <solmuscanbatuhan@gmail.com>
2023-08-08 12:30:24 +0300

    Update BlogList.js

--- src/BlogList.js
+++ src/BlogList.js
@@ -7,7 +7,7 @@ const BlogList = ({blogs,title}) => {
 
         {blogs.map((blog) =>(
             <div className="blog-preview" key={blog.id}>
-                <Link to={`https://json-test-mu.vercel.app/blogs/${blog.id}`}>
+                <Link to={`/blogs/${blog.id}`}>
                 <h2> {blog.title} </h2>
                 <p> Written by {blog.author} </p>
                 </Link>
